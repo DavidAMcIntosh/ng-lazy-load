1fc3082f16171ca0d9cb87f96802aceecf461cce
DavidAMcIntosh <davidmcintoshbz95@gmail.com>
2019-01-04 16:59:00 -0600

    create lazy loaded components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+{
+  path: "", 
+  component: HomeComponent
+},
+{
+  path: "viewdetails", 
+  loadChildren: "app/viewdetails/viewdetails.module#ViewDetailsModule"
+},
+{
+  path: "about", 
+  loadChildren: "app/about/about.module#AboutModule"
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
